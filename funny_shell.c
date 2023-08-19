752f8420888e5f065b0b8d51ac92b29ce5c9dbeb
DouibaOmar <douibaomar40@gmail.com>
2023-08-19 09:18:58 -0700

    Done

--- funny_shell.c
+++ funny_shell.c
@@ -1,43 +1,48 @@
 #include "main.h"
 
 /**
- *funny_shell - The main funtion for shell.
+ *funny_shell - The main funtion of the shell.
  *it will execute any command that enter by
  *the user.
  *Return: 0 in success.
  */
-int funny_shell(void)
+void funny_shell(void)
 {
 	char *line = NULL, *path_name = NULL;
 	char **line_arr = NULL, **path_arr = NULL;
+	int eixt_stat = 0;
 
+	/*Set up signal handler for Ctrl+C (SIGINT)*/
 	signal(SIGINT, handle_signal);
+	/*Get the value of the "PATH" environment variable*/
 	path_name = get_env("PATH");
+	/*Split the "PATH" into an array of directories*/
 	path_arr = split_path(path_name);
 	while (true)
 	{
-		prompt_cmd();
-		read_cmdline(path_name, path_arr, &line);
-		if (*line == '\n')
+		prompt_cmd(); /*Display the command prompt*/
+		line = read_cmdline(path_name, path_arr);
+		if (*line == '\n') /*If empty, skip the continue*/
 		{
 			free(line);
 			continue;
 		}
 		if (strncmp(line, "exit", 4) == 0)
-		{
-			free(line);
-			exit(EXIT_SUCCESS);
-		}
+		{ /*Check if the user entered the "exit" command*/
+			free(line); /*Free allocated memory*/
+			free(path_name);
+			free(path_arr);
+			exit(eixt_stat); /*Exit,with the recorded exit status*/
+		} /*Check if the user entered the "env" command*/
 		else if (strncmp(line, "env", 3) == 0)
-		{
+		{ /*Display environment variables*/
 			full_env(line);
-		}
+		} /*Handle other commands*/
 		else
-		{
+		{ /*Tokenize the command line*/
 			line_arr = line_to_tokens(line);
-			checker(line_arr, path_arr);
-			free(line);
+			eixt_stat = checker(line_arr, path_arr);
+			free(line); /*Free allocated memory */
 		}
 	}
-	return (EXIT_SUCCESS);
 }
---
+++ get_env.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/*
+ *_getenv - Funtion that return the envirement
+ *@str: variable name.
+ *Return: The path, otherise NULL.
+ */
+char *get_env(char *str)
+{
+	int i = 0, t_or_f = 0;
+	char *path_name = NULL;
+
+	/*Loop through each environment variable*/
+	for (i = 0; environ[i]; i++)
+	{
+		t_or_f = true_false(str, i);
+		/*If the names match, copy the environment variable's value*/
+		if (t_or_f == 0)
+		{ /*Copy the environment variable's value*/
+			path_name = strdup(environ[i]);
+			if (path_name == NULL)
+				exit(EXIT_FAILURE);
+			/*Return the value*/
+			return (path_name);
+		}
+	}
+
+	/*Return NULL if the environment variable is not found*/
+	return (NULL);
+}
+
+/**
+ *true_false -
+ *@str:
+ *@i:
+ *Return:
+ */
+bool true_false(const char *str, int i)
+{
+	int j = 0;
+	/*Compare the characters until '=' to the provided name*/
+	while (environ[i][j] != '=')
+	{
+		if (environ[i][j] != str[j])
+		{
+			return (1);
+			break;
+		}
+		else
+			return (0);
+		j++;
+	}
+}
