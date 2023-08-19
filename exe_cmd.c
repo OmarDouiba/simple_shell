69781a5b5ddfcb116085d4c331811a45650fa5bb
gonhis <h11mh12is@gmail.com>
2023-08-19 09:55:08 -0700

    functions exe_cmd.c and get_local.c done

---
+++ exe_cmd.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * execmd - fuction that handling the command execution
+ *
+ * @**argv: is an array of strings,where each string represents
+ * an argument for the command that needs to be executed
+ *
+ * Return: void
+ */
+
+void execmd(char **argv)
+{
+	char *command = NULL, *new_command = NULL;
+
+	if (argv)
+	{
+		command = argv[0];
+		new_command = get_location(command);
+
+		if (execve(new_command, argv, NULL) == -1)
+		{
+			perror("Error");
+		}
+	}
+}
