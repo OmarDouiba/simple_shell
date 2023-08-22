6c1aa7ee52f89998c07ee4cfd6159a895225ad82
gonhis <h11mh12is@gmail.com>
2023-08-22 08:43:20 -0700

    simple_shell done

--- own_getline.c
+++ own_getline.c
@@ -9,8 +9,7 @@ char *own_getline()
 	static char buffer[BUFFER_SIZE];
 	static int index;
 	static int bytes_read;
-	int start_pos;
-	int i;
+	int start_pos, i, nchars_read;
 
 	if (index >= bytes_read)
 	{
@@ -31,13 +30,14 @@ char *own_getline()
 		return (NULL);
 	}
 	buffer[index++] = '\0';
-	char *line = malloc(index - start_pos);
+	char *line = (char *) malloc(index - start_pos);
 
 	if (line == NULL)
 	{
 		return (NULL);
 	}
-	for (int i = start_pos; i < index; i++)
+	nchars_read = index - start_pos;
+	for (i = start_pos; i < index; i++)
 	{
 		line[i - start_pos] = buffer[i];
 	}
